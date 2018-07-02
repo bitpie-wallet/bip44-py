89142b2e3a0f6f597fb9342469cf8e2326dd6af1
zhumingu <zhumingu@163.com>
2018-07-02 14:29:19 +0800

    add bip44-py script

---
+++ bip44-py.py
@@ -0,0 +1,21 @@
+from bip32utils import BIP32Key, BIP32_HARDEN
+from mnemonic import Mnemonic
+
+# BTC  https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+coin_type = 0
+# 0: receive, 1: change
+path = 0
+# index
+index = 0
+
+seed = 'pride pride pride pride pride pride pride pride pride pride pride pride'
+
+m = BIP32Key.fromEntropy(Mnemonic.to_seed(seed))
+
+m = m.ChildKey(44 + BIP32_HARDEN)
+m = m.ChildKey(coin_type + BIP32_HARDEN)
+m = m.ChildKey(0 + BIP32_HARDEN)
+m = m.ChildKey(path)
+m = m.ChildKey(index)
+
+print m.dump()
